7ab4237263d47ecab7e175e0aea4fd36972d5d9b
Joris TRUONG <joris.truong@protonmail.com>
2021-01-02 22:33:03 +0100

    feat: added one page word sheet generation

--- src/components/generateOptions.js
+++ src/components/generateOptions.js
@@ -1,11 +1,13 @@
-import React, { useState, useRef, useEffect } from "react"
+import React, { useState } from "react"
 import { Card, Input, AutoComplete, Modal, Button, Typography, InputNumber, Select, Row, Col } from "antd"
 import { PlusOutlined, MinusOutlined } from "@ant-design/icons"
 import { AutoSizer, Collection } from "react-virtualized"
+import { jsPDF } from "jspdf"
+import "svg2pdf.js"
 
 
 const HanziWriter = require("hanzi-writer")
-const jsonQuery = require('json-query')
+const jsonQuery = require("json-query")
 
 const hanziData = require("../resources/hanzi.json")
 const hanziDataQuery = { "characters": hanziData }
@@ -28,11 +30,11 @@ const { Option } = Select;
 const GenerateOptions = () => {  
   const [suggestions, setSuggestions] = useState([])
   const [isModalVisible, setIsModalVisible] = useState(false)
-  const [inputs, setInputs] = useState([''])
+  const [inputs, setInputs] = useState([""])
   const [fannings, setFannings] = useState([0])
   const [placeholders, setPlaceholders] = useState([0])
-  const [directions, setDirections] = useState(["lrt"])
-  const [pages, setPages] = useState([1])
+  const [direction, setDirection] = useState("lrt")
+  const [lines, setLines] = useState([1])
   const [inputIndex, setInputIndex] = useState(0)
 
   const handleSearch = (value) => {
@@ -76,8 +78,8 @@ const GenerateOptions = () => {
             label: (
               <div
                 style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
+                  display: "flex",
+                  justifyContent: "space-between",
                 }}
               >
                 <span>
@@ -138,7 +140,7 @@ const GenerateOptions = () => {
               Practice handwriting
             </Title>
             <Paragraph style={{ textAlign: "justify" }}>
-              Learning chinese characters stroke order is cool and all... But don't you ever want to go back to the roots? You can generate word sheet to print for handwriting practice here. The word sheets are customizable: you can choose to get stroke order help, change the writing direction to left-to-right or top-to-bottom, select the number of pages... Whatever your level is, you can generate word sheets that correpond to you! (Do not forget to save the forests.)
+              Learning chinese characters stroke order is cool and all... But don"t you ever want to go back to the roots? You can generate word sheet to print for handwriting practice here. The word sheets are customizable: you can choose to get stroke order help, change the writing direction to left-to-right or top-to-bottom, select the number of lines... Whatever your level is, you can generate word sheets that correpond to you! (Do not forget to save the forests.)
             </Paragraph>
           </Typography>
         </Col>
@@ -147,10 +149,10 @@ const GenerateOptions = () => {
         inputs.map((element, index) => {
           return(
             <Row key={`generate ${index}`} style={{ paddingBottom: 25 }}>
-              <Col lg={{ span: 18, offset: 3 }} md={{ span: 18, offset: 3 }} xs={{ span: 24 }}>
+              <Col lg={{ span: 14, offset: 5 }} md={{ span: 18, offset: 3 }} xs={{ span: 24 }}>
                 <Card>
                   <Row gutter={[16, 16]}>
-                    <Col lg={{ span: 8}} xs={{ span: 24 }}>
+                    <Col lg={{ span: 9}} xs={{ span: 24 }}>
                       <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Col span={24} style={{ textAlign: "center" }}>
                           <strong>Character</strong>
@@ -188,7 +190,7 @@ const GenerateOptions = () => {
                         </Col>
                       </Row>
                     </Col>
-                    <Col lg={{ span: 4}} xs={{ span: 24 }}>
+                    <Col lg={{ span: 5}} xs={{ span: 24 }}>
                       <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Col span={24} style={{ textAlign: "center" }}>
                           <strong>Stroke fanning help</strong>
@@ -207,7 +209,7 @@ const GenerateOptions = () => {
                         </Col>
                       </Row>
                     </Col>
-                    <Col lg={{ span: 4}} xs={{ span: 24 }}>
+                    <Col lg={{ span: 5}} xs={{ span: 24 }}>
                       <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Col span={24} style={{ textAlign: "center" }}>
                           <strong>Character placeholder</strong>
@@ -226,41 +228,20 @@ const GenerateOptions = () => {
                         </Col>
                       </Row>
                     </Col>
-                    <Col lg={{ span: 4}} xs={{ span: 24 }}>
+                    <Col lg={{ span: 5}} xs={{ span: 24 }}>
                       <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Col span={24} style={{ textAlign: "center" }}>
-                          <strong>Direction</strong>
-                        </Col>
-                        <Col span={24}>
-                          <Select
-                            style={{ width: "100%" }}
-                            defaultValue="ltr"
-                            onChange={(value) => {
-                              let updatedDirections = [...directions]
-                              updatedDirections[index] = value
-                              setDirections(updatedDirections)
-                            }}
-                          >
-                            <Option value="ltr">Left to right</Option>
-                            <Option value="ttb">Top to bottom</Option>
-                          </Select>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col lg={{ span: 4}} xs={{ span: 24 }}>
-                      <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <Col span={24} style={{ textAlign: "center" }}>
-                          <strong>Number of pages</strong>
+                          <strong>Number of lines</strong>
                         </Col>
                         <Col span={24}>
                           <InputNumber
                             min={1}
                             style={{ width: "100%" }}
-                            value={pages[index]}
+                            value={lines[index]}
                             onChange={(value) => {
-                              let updatedPages = [...pages]
-                              updatedPages[index] = value
-                              setPages(updatedPages)
+                              let updatedLines = [...lines]
+                              updatedLines[index] = value
+                              setLines(updatedLines)
                             }}
                           />
                         </Col>
@@ -285,12 +266,9 @@ const GenerateOptions = () => {
             let updatedPlaceholders = [...placeholders]
             updatedPlaceholders[placeholders.length] = 0
             setPlaceholders(updatedPlaceholders)
-            let updatedPages = [...pages]
-            updatedPages[pages.length] = 1
-            setPages(updatedPages)
-            let updatedDirections = [...directions]
-            updatedDirections[directions.length] = "ltr"
-            setDirections(updatedDirections)
+            let updatedLines = [...lines]
+            updatedLines[lines.length] = 1
+            setLines(updatedLines)
           }} />
         </Col>
         <Col lg={{span: 2}} md={{span: 2}} xs={{span: 6}}>
@@ -307,22 +285,159 @@ const GenerateOptions = () => {
             let updatedPlaceholders = [...placeholders]
             updatedPlaceholders.pop()
             setPlaceholders(updatedPlaceholders)
-            let updatedPages = [...pages]
-            updatedPages.pop()
-            setPages(updatedPages)
-            let updatedDirections = [...directions]
-            updatedDirections.pop()
-            setDirections(updatedDirections)
+            let updatedLines = [...lines]
+            updatedLines.pop()
+            setLines(updatedLines)
           }} />
         </Col>
       </Row>
       <Row style={{ textAlign: "center" }} gutter={[16, 16]}>
         <Col lg={{ span: 2, offset: 11 }} md={{ span: 6, offset: 4 }} xs={{ span: 9 }}>
-          <Button type="primary" onClick={() => {console.log(inputs); console.log(fannings); console.log(placeholders); console.log(directions); console.log(pages)}} style={{ width: "100%" }}>Generate</Button>
+          <Button type="primary" style={{ width: "100%" }} onClick={() => {
+            var target = document.createElement("target");
+            var svgText = `<svg width="210mm" height="297mm">`
+            for (var i = 0; i < lines.reduce((a, b) => a + b); i++) {
+              svgText = svgText + horizontalLineCharacterBackground(i)
+            }
+            var position = 0
+            var startX = 25
+            var startY = 95
+            var promises = []
+            inputs.forEach((input, index) => {
+              promises.push(
+                HanziWriter.loadCharacterData(input).then(function(charData) {
+                  var i = 0 // iterator for each fanning
+                  svgText = `${svgText} ${renderFanningStrokes(startX, startY, charData.strokes, true)}`
+                  startX = startX + 75
+                  position = position + 1
+                  i = i + 1
+                  for (var j = 0; j < fannings[index]; j++) { // j is an iterator for number of times of fanning help
+                    while (i <= charData.strokes.length && position < 10 * lines[index]) {
+                      var strokesPortion = charData.strokes.slice(0, i);
+                      if (startX === 775) {
+                        startX = 25
+                        startY = startY + 75
+                      }
+                      svgText = `${svgText} ${renderFanningStrokes(startX, startY, strokesPortion, false)}`
+                      startX = startX + 75
+                      position = position + 1
+                      i = i + 1
+                    }
+                    i = 1
+                  }
+                  var k = 0 // iterator for number of times of placeholder help
+                  while (k < placeholders[index] && position < 10 * lines[index]) {
+                    if (startX === 775) {
+                      startX = 25
+                      startY = startY + 75
+                    }
+                    svgText = `${svgText} ${renderFanningStrokes(startX, startY, charData.strokes, false)}`
+                    startX = startX + 75
+                    position = position + 1
+                    k = k + 1
+                  }
+                }).then(() => {
+                  startX = 25
+                  startY = startY + 75
+                  position = 0
+                })
+              )
+            })
+            Promise.all(promises).then(() => {
+              target.innerHTML = svgText + "</svg>"
+              const svgElement = target.firstElementChild
+              svgElement.getBoundingClientRect()
+              const width = svgElement.width.baseVal.value
+              const height = svgElement.height.baseVal.value
+              const pdf = new jsPDF(width > height ? "l" : "p", "pt", [width, height])
+              pdf.svg(svgElement, { width, height })
+                .then(() => {
+                  pdf.save("practice.pdf")
+                })
+            })
+          }}>Generate</Button>
+        </Col>
+      </Row>
+      <Row style={{ textAlign: "center" }} gutter={[16, 16]}>
+        <Col lg={{ span: 4, offset: 10 }} xs={{ span: 24 }}>
+          <Row style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <Col span={24} style={{ textAlign: "center" }}>
+              <strong>Direction</strong>
+            </Col>
+            <Col span={24}>
+              <Select
+                style={{ width: "100%" }}
+                defaultValue="ltr"
+                onChange={(value) => {
+                  setDirection(value)
+                }}
+              >
+                <Option value="ltr">Left to right</Option>
+                <Option value="ttb">Top to bottom</Option>
+              </Select>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
   )
 }
+
+function renderFanningStrokes(startX, startY, strokes, start) {
+  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  // set the transform property on the g element so the character renders at 75x75
+  var transformData = HanziWriter.getScalingTransform(75, 75);
+  group.setAttributeNS(null, "transform", `translate(${startX}, ${startY}) scale(${transformData.scale}, -${transformData.scale})`);
+
+  strokes.forEach(function(strokePath) {
+    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttributeNS(null, "d", strokePath);
+    // style the character paths
+    path.style.fill = start ? "#555" : "#DDD";
+    group.appendChild(path);
+  });
+
+  return group.outerHTML;
+}
+
+// Max line is 13
+function horizontalLineCharacterBackground(line) {
+  var background = ""
+  for (var i = 0; i < 10; i++) {
+    background = background +
+    `
+      <line x1="${(25 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(100 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#DDD" />
+      <line x1="${(100 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(25 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#DDD" />
+      <line x1="${(62.5 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(62.5 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#DDD" />
+      <line x1="${(25 + 75 * i)}" y1="${(67.5 + 75 * line)}" x2="${(100 + 75 * i)}" y2="${(67.5 + 75 * line)}" stroke="#DDD" />
+      <line x1="${(25 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(100 + 75 * i)}" y2="${(30 + 75 * line)}" stroke="#555" />
+      <line x1="${(25 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(25 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#555" />
+      <line x1="${(25 + 75 * i)}" y1="${(105 + 75 * line)}" x2="${(100 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#555" />
+      <line x1="${(100 + 75 * i)}" y1="${(30 + 75 * line)}" x2="${(100 + 75 * i)}" y2="${(105 + 75 * line)}" stroke="#555" />
+    `
+  }
+  return background
+}
+
+// Max line is 9
+function verticalLineCharacterBackground(line) {
+  var background = ""
+  for (var i = 0; i < 14; i++) {
+    background = background +
+    `
+      <line x1="${700 - 75 * line}" y1="${30 + 75 * i}" x2="${775 - 75 * line}" y2="${105 + 75 * i}" stroke="#DDD" />
+      <line x1="${775 - 75 * line}" y1="${30 + 75 * i}" x2="${700 - 75 * line}" y2="${105 + 75 * i}" stroke="#DDD" />
+      <line x1="${737.5 - 75 * line}" y1="${30 + 75 * i}" x2="${737.5 - 75 * line}" y2="${105 + 75 * i}" stroke="#DDD" />
+      <line x1="${700 - 75 * line}" y1="${67.5 + 75 * i}" x2="${775 - 75 * line}" y2="${67.5 + 75 * i}" stroke="#DDD" />
+      <line x1="${700 - 75 * line}" y1="${30 + 75 * i}" x2="${775 - 75 * line}" y2="${30 + 75 * i}" stroke="#555" />
+      <line x1="${700 - 75 * line}" y1="${30 + 75 * i}" x2="${700 - 75 * line}" y2="${105 + 75 * i}" stroke="#555" />
+      <line x1="${700 - 75 * line}" y1="${105 + 75 * i}" x2="${775 - 75 * line}" y2="${105 + 75 * i}" stroke="#555" />
+      <line x1="${775 - 75 * line}" y1="${30 + 75 * i}" x2="${775 - 75 * line}" y2="${105 + 75 * i}" stroke="#555" />
+    `
+  }
+  return background
+}
+      
 
 export default GenerateOptions
